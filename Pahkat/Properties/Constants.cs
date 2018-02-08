0a856c065f6699f5b482d62f2e7e9026e18a3030
Brendan Molloy <brendan@bbqsrc.net>
2018-02-09 00:40:03 +0100

    Update URL constants

--- Pahkat/Properties/Constants.cs
+++ Pahkat/Properties/Constants.cs
@@ -6,7 +6,7 @@ namespace Pahkat.Properties
     public static class Constants
     {
         public const string RegistryId = "Pahkat";
-        public const string Repository = "http://localhost:8000/";
+        public const string Repository = "https://x.brendan.so/test-repo/";
         public const PeriodInterval UpdateCheckInterval = PeriodInterval.Daily;
         public const string Channel = "Stable";
 
@@ -14,6 +14,6 @@ namespace Pahkat.Properties
             "https://dd5eed16a4674ba883f002d93d067f65:9412c45254514e4d8a10dba63607d2fc@sentry.io/260424";
 
         public static Uri TaskbarIcon = new Uri("pack://application:,,,/UI/TaskbarIcon.ico");
-        public static Uri BahkatUpdateUri = new Uri("https://repo.bahkat.org");
+        public static Uri PahkatUpdateUri = new Uri("https://repo.bahkat.org/windows");
     }
 }
