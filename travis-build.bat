537bf349ffb7a329b0924232e84d50d08e808c25
Martin Carltoft <carltoft@technocreatives.com>
2019-01-10 12:50:50 +0100

    Add current directory to inno setup call pfx path

--- travis-build.bat
+++ travis-build.bat
@@ -2,7 +2,7 @@
 setlocal
 setlocal enableDelayedExpansion
 set "PFX_PASSWORD=1234"
-set "PFX=C:\Users\Martin\source\repos\pahkat-client-windows\tmp.pfxxx"
+set "PFX=tmp.pfxxx"
 set "BIN=DivvunInstaller.exe"
   set PATH=%PATH%;c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;c:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool
   call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
@@ -12,5 +12,5 @@ set "BIN=DivvunInstaller.exe"
   signtool sign /debug /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% /d "Divvun Installer" .\Pahkat\bin\x86\Release\Pahkat.Sdk.dll || exit /b !ERRORLEVEL!
   signtool sign /debug /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% /d "Divvun Installer" .\Pahkat\bin\x86\Release\pahkat_client.dll || exit /b !ERRORLEVEL!
   signtool sign /debug /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% /d "Divvun Installer" .\Pahkat\bin\x86\Release\%BIN% || exit /b !ERRORLEVEL!
-  "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp /O.\output /S"signtool=signtool.exe sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% $f" setup.iss
+  "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp /O.\output /S"signtool=signtool.exe sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f %cd%\%PFX% /p %PFX_PASSWORD% $f" setup.iss
 endlocal
