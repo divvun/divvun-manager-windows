e9d363783359989d1cd1ef22f0bff8755dda8f23
Martin Carltoft <carltoft@technocreatives.com>
2019-01-10 12:21:17 +0100

    Remove bad cd

--- travis-build.bat
+++ travis-build.bat
@@ -4,8 +4,6 @@ setlocal enableDelayedExpansion
 set "PFX_PASSWORD=1234"
 set "PFX=C:\Users\Martin\source\repos\pahkat-client-windows\tmp.pfxxx"
 set "BIN=DivvunInstaller.exe"
-  echo %cd%
-  cd C:\Users\travis\build\divvun\pahkat-client-windows
   set PATH=%PATH%;c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;c:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool
   call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
   rem nuget restore || exit /b !ERRORLEVEL!
@@ -15,5 +13,4 @@ set "BIN=DivvunInstaller.exe"
   signtool sign /debug /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% /d "Divvun Installer" .\Pahkat\bin\x86\Release\pahkat_client.dll || exit /b !ERRORLEVEL!
   signtool sign /debug /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% /d "Divvun Installer" .\Pahkat\bin\x86\Release\%BIN% || exit /b !ERRORLEVEL!
   "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" /Qp /O.\output /S"signtool=signtool.exe sign /t http://timestamp.verisign.com/scripts/timstamp.dll /f %PFX% /p %PFX_PASSWORD% $f" setup.iss
-  pause
 endlocal
