740671f01dfd9373fbd92642cf0224e292ed296d
Brendan Molloy <brendan@bbqsrc.net>
2019-07-19 21:41:24 +0200

    A clean start for good health

--- Pahkat.Sdk/Properties/AssemblyInfo.cs
+++ Pahkat.Sdk/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Divvun Installer Sdk")]
+[assembly: AssemblyTitle("Pahkat Client SDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Divvun Installer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Pahkat")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
