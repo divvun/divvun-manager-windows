8d472a8e940eeb69b5261e4368ee44c62ca15bd1
Brendan Molloy <brendan@bbqsrc.net>
2020-09-08 13:09:58 +0200

    Update DSN

--- Divvun.Installer/Properties/Constants.cs
+++ Divvun.Installer/Properties/Constants.cs
@@ -6,7 +6,7 @@ namespace Divvun.Installer.Properties
     public static class Constants
     {
         public const string SentryDsn =
-            "https://4022315e99574f87bad3ef6600e0c846@o157567.ingest.sentry.io/1357465";
+            "https://48f57ed5a85e4450876c4a8d209e2f7e@o157567.ingest.sentry.io/1357465";
         public static Uri TaskbarIcon = new Uri("pack://application:,,,/divvun-logo-512.ico");
     }
 }
