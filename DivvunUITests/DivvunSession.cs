becfbf5d6f5c0d1d2c4362a6f7450956b99ea82b
Zoey Riordan <zoey@dos.cafe>
2020-10-27 14:19:33 +0100

    set a reasonable default path

--- DivvunUITests/DivvunSession.cs
+++ DivvunUITests/DivvunSession.cs
@@ -12,7 +12,7 @@ namespace DivvunUITests
         public DivvunSession()
         {
             var appiumOptions = new OpenQA.Selenium.Appium.AppiumOptions();
-            appiumOptions.AddAdditionalCapability("app", @"F:\Dev\divvun-manager-windows\Divvun.Installer\bin\x86\Release\DivvunInstaller.exe");
+            appiumOptions.AddAdditionalCapability("app", @"C:\DivvunManager\DivvunInstaller.exe");
             this.Session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
             this.Session.SwitchTo().Window(this.Session.WindowHandles[0]);
 
