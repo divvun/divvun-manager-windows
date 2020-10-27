219e6505f8a6749b537999a92529ad02039478ae
Zoey Riordan <zoey@dos.cafe>
2020-10-27 14:12:00 +0100

    add uninstall test

---
+++ DivvunUITests/DivvunSession.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DivvunUITests
+{
+    public abstract class DivvunSession : IDisposable
+    {
+        protected readonly WindowsDriver<WindowsElement> Session;
+
+        public DivvunSession()
+        {
+            var appiumOptions = new OpenQA.Selenium.Appium.AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", @"F:\Dev\divvun-manager-windows\Divvun.Installer\bin\x86\Release\DivvunInstaller.exe");
+            this.Session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+        }
+
+        public void Dispose()
+        {
+            this.Session.Close();
+            this.Session.Quit();
+        }
+    }
+}
--- DivvunUITests/InstallTests.cs
+++ DivvunUITests/InstallTests.cs
@@ -5,11 +5,13 @@ using System;
 using System.Threading;
 using Xunit;
 
-namespace DivvunUITest
+namespace DivvunUITests
 {
-    public class InstallTests
+    [TestCaseOrderer("DivvunUITests.PriorityOrderer", "DivvunUITests")]
+    public class InstallTests : DivvunSession
     {
         [Fact]
+        [TestPriority(1)]
         public void TestInstall()
         {
             var appiumOptions = new OpenQA.Selenium.Appium.AppiumOptions();
@@ -36,5 +38,32 @@ namespace DivvunUITest
             Assert.Equal("Installed", installedText);
         }
 
+        [Fact]
+        [TestPriority(2)]
+        public void TestUninstall()
+        {
+            var appiumOptions = new OpenQA.Selenium.Appium.AppiumOptions();
+            appiumOptions.AddAdditionalCapability("app", @"F:\Dev\divvun-manager-windows\Divvun.Installer\bin\x86\Release\DivvunInstaller.exe");
+            var divvunSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appiumOptions);
+
+            divvunSession.SwitchTo().Window(divvunSession.WindowHandles[0]);
+            divvunSession.FindElementByAccessibilityId("TitleBarReposButton").SendKeys(Keys.Return);
+            divvunSession.FindElementByName("All Repositories").SendKeys(Keys.Return);
+
+            var treeItem = divvunSession.FindElementsByName("Divvun.Installer.UI.Shared.PackageCategoryTreeItem")[3];
+            treeItem.SendKeys(Keys.Space);
+
+            divvunSession.FindElementByAccessibilityId("BtnPrimary").SendKeys(Keys.Enter);
+
+            Thread.Sleep(5000);
+            divvunSession.FindElementByAccessibilityId("BtnFinish").SendKeys(Keys.Enter);
+
+            var installedText = divvunSession
+                .FindElementsByName("Divvun.Installer.UI.Shared.PackageCategoryTreeItem")[3]
+                .FindElementsByName("Divvun.Installer.UI.Shared.PackageMenuItem")[0]
+                .FindElementsByClassName("TextBlock")[3].Text;
+
+            Assert.Equal("Not Installed", installedText);
+        }
     }
 }
