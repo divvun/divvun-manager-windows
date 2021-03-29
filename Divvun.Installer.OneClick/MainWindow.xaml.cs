69f942a68a4f18d8b4817b50f18800aa56781bcd
Zoey Riordan <zoey@dos.cafe>
2021-03-29 11:49:50 +0200

    bundle 64-bit kbdi because windows

--- Divvun.Installer.OneClick/MainWindow.xaml.cs
+++ Divvun.Installer.OneClick/MainWindow.xaml.cs
@@ -297,6 +297,17 @@ namespace Divvun.Installer.OneClick
                 Console.WriteLine("Starting install process");
                 await pahkat.ProcessTransaction(actions, (message) =>
                 {
+                    if (message.IsT0)
+                    {
+                        var progress = message.AsDownloadProgress!;
+                        DownloadProgresBar.IsIndeterminate = false;
+                        DownloadProgresBar.Maximum = progress.Total;
+                        DownloadProgresBar.Value = progress.Current;
+                    } else
+                    {
+                        DownloadProgresBar.IsIndeterminate = true;
+                    }
+
                     Console.WriteLine(message);
                     if (message.IsErrorState)
                     {
@@ -347,7 +358,16 @@ namespace Divvun.Installer.OneClick
                 throw new Exception("No matching language found in meta.");
             }
 
-            var kbdiFile = System.IO.Path.Join(Directory.GetCurrentDirectory(), "kbdi.exe");
+            string kbdiFile;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                kbdiFile = System.IO.Path.Join(Directory.GetCurrentDirectory(), "kbdi-x64.exe");
+            }
+            else
+            {
+                kbdiFile = System.IO.Path.Join(Directory.GetCurrentDirectory(), "kbdi.exe");
+            }
+
 
             var regionCode = RegionInfo.CurrentRegion.TwoLetterISORegionName;
 
